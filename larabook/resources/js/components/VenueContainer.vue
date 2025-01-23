f250be935137c6a59a20e54572c65d96d38501ab
andreas jakobsen <volvo240jakobsen@gmail.com>
2025-01-23 01:59:19 +0100

    adding flex wrap and magin

--- larabook/resources/js/components/VenueContainer.vue
+++ larabook/resources/js/components/VenueContainer.vue
@@ -13,11 +13,11 @@ onMounted(getVenues());
 </script>
 
 <template>
-  <div class="flex gap-3 justify-center">
+  <div class="flex flex-wrap gap-3 justify-center">
     <div v-for="venue in venues" class="w-96 h-96 border border-sky-500 shadow-lg rounded-xl bg-white">
       <h3 class="text-center m-1">{{ venue.name }}</h3>
       <img :src="venue.image" class="object-cover h-48 w-96">
-      <div class="flex justify-between">
+      <div class="flex justify-between mt-1">
         <h4 class="ml-8">{{ venue.price }} NOK</h4>
         <h4 class="mr-4">{{ venue.city }}</h4>
       </div>
