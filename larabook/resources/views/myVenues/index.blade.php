0c0ffdb43b6d02e675a70b0654dd723bcab33169
andreas jakobsen <volvo240jakobsen@gmail.com>
2025-03-18 20:24:34 +0100

    fixed problem with only one vue component displaying on my venues page.

--- larabook/resources/views/myVenues/index.blade.php
+++ larabook/resources/views/myVenues/index.blade.php
@@ -1,11 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        My Venues
+        My Venues & products 
+        <x-nav-link href="/venue/create">Add venue</x-nav-link>
+        <x-nav-link href="/products/create">Add Product</x-nav-link>
     </x-slot:heading>
-    <x-nav-link href="/venue/create">Add venue</x-nav-link>
-    <x-nav-link href="/products/create">Add Product</x-nav-link>
-    <h3>Venues</h3>
-    <!-- <My-Venues /> -->
-    <h3>Products</h3>
+    <h3 class="text-center m-6">Venues</h3>
+    <div>
+    <My-Venues><My-Venues />
+    </div>
+    <h3 class="text-center m-6">Products</h3>
     <List-Products />
 </x-layout>
--- larabook/resources/views/venues/create.blade.php
+++ larabook/resources/views/venues/create.blade.php
@@ -79,7 +79,7 @@
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
     <a href="/" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
   </div>
 </form>
 
